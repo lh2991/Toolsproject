65d12e635e2309c474817e6669c56f4809dc91d6
lh2991 <lh2991@columbia.edu>
2019-12-02 20:09:32 -0500

    added all.html

--- toolssq/sightings/urls.py
+++ toolssq/sightings/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'index'),
-    # path('', views.index, name= 'index'),
+        path('list/',views.all_squirrel),
+        path('<int: Squirrel_ID >/',views.squirrel_details),
 ]
