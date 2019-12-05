951695edf0a4b55e169f8568556c2b1ee1e69c07
lh2991 <lh2991@columbia.edu>
2019-12-05 13:01:33 -0500

    Added stats html and page and link

--- toolssq/sightings/urls.py
+++ toolssq/sightings/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
         path('',views.all_squirrel),
         path('<squirrel_id>/',views.squirrel_details),
-        path('add',views.add_squirrel)
+        path('add',views.add_squirrel),
+        path('stats',views.stats),
 ]
